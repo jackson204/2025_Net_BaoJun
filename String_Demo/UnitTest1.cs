012e3c5a02451168153f8194ba722d04bb1ad1f9
jackson <s97403002a@gmail.com>
2025-02-13 20:00:33 +0800

    String Demo

--- String_Demo/UnitTest1.cs
+++ String_Demo/UnitTest1.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace String_Demo;
 
 public class UnitTest1
@@ -5,5 +7,20 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var msg = "Cahina";
+        msg = "China";
+        msg.Should().Be("China");
     }
+
+    [Fact]
+    public void Test2()
+    {
+        var a = "Hello";
+        var b = "World";
+        var c = a + b;
+        var result = c == "HelloWorld";
+        result.Should().BeTrue();
+    }
+
+    
 }
