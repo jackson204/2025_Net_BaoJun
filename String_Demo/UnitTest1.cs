da6a4b85edbbb741e196ff67a86f4c12efa45e9a
jackson <s97403002a@gmail.com>
2025-02-13 22:03:33 +0800

    add default Test

--- String_Demo/UnitTest1.cs
+++ String_Demo/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Xml;
 using FluentAssertions;
 
 namespace String_Demo;
@@ -22,5 +23,46 @@ public class UnitTest1
         result.Should().BeTrue();
     }
 
-    
+    [Fact]
+    public void Test3()
+    {
+        string a = default;
+        a.Should().Be(null);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        string? a = default;
+        a.Should().Be(null);
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        int? a = default;
+        int b = default;
+        a.Should().Be(null);
+        b.Should().Be(0);
+    }
+
+    [Fact]
+    public void Test6()
+    {
+        int a = 1;
+        int? b = null;
+        (a - b).Should().Be(null);
+
+    }
+
+    [Fact]
+    public void Test7()
+    {
+        DateTime a = new DateTime(2021, 1, 1);
+        DateTime b = default;
+        DateTime? c = null;
+        a.Should().Be(new DateTime(2021, 1, 1));
+        b.Should().Be(new DateTime(0001, 01, 01));
+        c.Should().Be(null);
+    }
 }
