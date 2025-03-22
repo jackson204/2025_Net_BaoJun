19c9b218837b3ef5d6146d091cc3b1a837186374
jackson <s97403002a@gmail.com>
2025-03-22 19:09:27 +0800

    add nuget DependencyInjection

---
+++ DI_Demo/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DI_Demo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ITestService testService = new TestService();
+        // testService.Name ="TestService";
+        // testService.SayHello();
+      
+        // 使用 serviceCollection();
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddScoped<ITestService,TestService2>();
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var testService = serviceProvider.GetService<ITestService>()!;
+        testService.Name = "TestService";
+        testService.SayHello();
+    }
+}
+
+interface ITestService
+{
+    public string Name { get; set; }
+    
+    public void SayHello();
+}
+
+class TestService : ITestService
+{
+    public string Name { get; set; }
+
+    public void SayHello()
+    {
+        Console.WriteLine($"Hello, {Name}");
+    }
+}
+
+class TestService2 : ITestService
+{
+    public string Name { get; set; }
+
+    public void SayHello()
+    {
+        Console.WriteLine($"Hello, {Name} from TestService2");
+    }
+}
