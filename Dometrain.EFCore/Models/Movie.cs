8067a7100422404beb1e2ebd4a9edededf5e843a
jackson <s97403002a@gmail.com>
2025-05-17 15:59:42 +0800

    init From Zero to Hero: Entity Framework Core in .NET

---
+++ Dometrain.EFCore/Models/Movie.cs
@@ -0,0 +1,9 @@
+namespace Dometrain.EFCore.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }    
+    public DateTime ReleaseDate { get; set; }
+    public string? Synopsis { get; set; }
+}
