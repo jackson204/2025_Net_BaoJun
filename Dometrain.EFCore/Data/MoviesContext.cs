53cc90d7eb14abcb5d118a50e6cac2afa62f17ba
jackson <s97403002a@gmail.com>
2025-05-17 21:32:50 +0800

    install Microsoft.EntityFrameworkCore.SqlServe and UseSqlServer

--- Dometrain.EFCore/Data/MoviesContext.cs
+++ Dometrain.EFCore/Data/MoviesContext.cs
@@ -5,5 +5,11 @@ namespace Dometrain.EFCore.Data;
 
 public class MoviesContext : DbContext
 {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=MoviesDb;User Id=sa;Password=YourStrong!Passw0rd;");
+        base.OnConfiguring(optionsBuilder);
+    }
+
     public DbSet<Movie> Movies => Set<Movie>();
 }
