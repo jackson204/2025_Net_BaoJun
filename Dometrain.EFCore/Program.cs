6b1bde36bdf6c65eca16f386c38b6009e76eb26a
jackson <s97403002a@gmail.com>
2025-05-17 22:12:36 +0800

    fix Invalid object name 'Movies'.

--- Dometrain.EFCore/Program.cs
+++ Dometrain.EFCore/Program.cs
@@ -9,8 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext with SQL Server
 builder.Services.AddDbContext<MoviesContext>();
 var app = builder.Build();
+
+var serviceScope = app.Services.CreateScope();
+var dbContext = serviceScope.ServiceProvider.GetRequiredService<MoviesContext>();
+
+dbContext.Database.EnsureDeleted();
+dbContext.Database.EnsureCreated();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
