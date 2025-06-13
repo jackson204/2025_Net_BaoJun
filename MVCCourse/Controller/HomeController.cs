db68055500db9879e228f5a1c7df65e9202b260f
jackson <temp@example.com>
2025-06-13 21:22:50 +0800

    add HomeController and  DI

---
+++ MVCCourse/Controller/HomeController.cs
@@ -0,0 +1,10 @@
+namespace MVCCourse.Controller;
+
+public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+{
+    // GET
+    public string Index()
+    {
+        return "Hello, World!";
+    }
+}
