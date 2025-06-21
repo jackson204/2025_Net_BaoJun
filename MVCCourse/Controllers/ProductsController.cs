73bb231ec13ab2f229d1f64637928efdcc1e1b6c
jackson <temp@example.com>
2025-06-21 17:16:38 +0800

    feat: 新增產品編輯功能及相關視圖，並在產品列表中添加編輯連結

--- MVCCourse/Controllers/ProductsController.cs
+++ MVCCourse/Controllers/ProductsController.cs
@@ -6,6 +6,29 @@ namespace MVCCourse.Controllers;
 
 public class ProductsController : Controller
 {
+    public IActionResult Edit(int id)
+    {
+        var productViewModel = new ProductViewModel
+        {
+            Product = ProductsRepository.GetProductById(id)?? new Product(),
+            Categories = CategoriesRepository.GetCategories()
+        };
+        return View(productViewModel);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(ProductViewModel productViewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            ProductsRepository.UpdateProduct(productViewModel.Product.ProductId,productViewModel.Product);
+            return RedirectToAction(nameof(Index));
+        }
+        productViewModel.Categories = CategoriesRepository.GetCategories();
+        return View(productViewModel); 
+    }
+    
+    
     public IActionResult Add()
     {
         var productViewModel = new ProductViewModel
